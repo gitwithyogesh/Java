2051225e4e5f2d5a3f183264021559ef98fd2aff
Yogesh Anup Shirke <yshirke005@gmail.com>
2025-12-04 19:09:28 +0530

    gg

--- TwoDArray.java
+++ TwoDArray.java
@@ -30,8 +30,9 @@ public class TwoDArray {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.println(matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
